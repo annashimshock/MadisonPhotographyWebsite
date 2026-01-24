afbeb8f16c59c2c4fd902f09efbb93ab1ccf4f5a
annashimshock <122415457+annashimshock@users.noreply.github.com>
2026-01-24 00:36:53 -0800

    fixed js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
-const images = document.querySelectorAll(".gallery img");
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
+document.addEventListener("DOMContentLoaded", () => {
+  // --- Lightbox ---
+  const images = document.querySelectorAll(".gallery img");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
 
-images.forEach(image => {
-  image.addEventListener("click", () => {
-    lightbox.style.display = "flex";
-    lightboxImg.src = image.src;
+  images.forEach(image => {
+    image.addEventListener("click", () => {
+      lightbox.style.display = "flex";
+      lightboxImg.src = image.src;
+    });
   });
-});
 
-lightbox.addEventListener("click", () => {
-  lightbox.style.display = "none";
-});
-
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
-
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('show');
-});
-
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.remove('show');
+  lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
   });
+
+  // --- Hamburger menu ---
+  const toggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav');
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+
+    // Close menu when a nav link is clicked
+    document.querySelectorAll('nav a').forEach(link => {
+      link.addEventListener('click', () => {
+        nav.classList.remove('show');
+      });
+    });
+  }
 });
